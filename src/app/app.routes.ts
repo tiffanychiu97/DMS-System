558b1f531acabaa1b746f1708b44beb45a2958a2
MSI\User <t1427t1427@gmail.com>
2026-07-20 18:27:38 +0800

    add defect-form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DefectList } from './features/defect-list/defect-list';
 import { Dashboard } from './features/dashboard/dashboard';
+import { DefectForm } from './features/defect-form/defect-form';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'defects', pathMatch: 'full' },
   { path: 'defects', component: DefectList },
   { path: 'dashboard', component: Dashboard },
+  { path: 'defect-form', component: DefectForm }
 ];
---
+++ src/app/features/defect-form/defect-form.ts
@@ -0,0 +1,55 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { DefectService } from '../../core/services/defect.service';
+import { DefectSeverity, DefectStatus } from '../../core/defect.model';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-defect-form',
+  imports: [ RouterLink, ReactiveFormsModule ],
+  templateUrl: './defect-form.html',
+  styleUrl: './defect-form.scss',
+})
+
+export class DefectForm {
+  private fb = inject(FormBuilder);
+  private defectService = inject(DefectService);
+  private router = inject(Router);
+
+  readonly severityOptions = Object.values(DefectSeverity);
+  readonly statusOptions = Object.values(DefectStatus);
+ 
+  readonly form = this.fb.group({
+    title: ['', [Validators.required, Validators.minLength(5)]],
+    description: ['', [Validators.required, Validators.minLength(10)]],
+    productId: ['', Validators.required],
+    station: ['', Validators.required],
+    severity: [DefectSeverity.Minor, Validators.required],
+    status: [DefectStatus.Open, Validators.required],
+    assignedTo: ['', Validators.required],
+    reportedBy: ['', Validators.required],
+  })
+
+   field(name: string) {
+    return this.form.get(name);
+  }
+
+  showError(name: string){
+     const control = this.field(name)
+     return !!control && control.invalid && (control.touched || control.dirty);
+  }
+
+  submit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.defectService.create(this.form.getRawValue() as any);
+    this.router.navigate(['/defects']);
+  }
+
+  cancel(): void{
+    this.router.navigate(['/defects'])
+  }
+}
