8553cd21f84631f29f05aa0b75804a51a831acc9
MSI\User <t1427t1427@gmail.com>
2026-07-20 14:30:17 +0800

    route setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DefectList } from './features/defect-list/defect-list';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'defects', pathMatch: 'full' },
+  { path: 'defects', component: DefectList },
+  { path: 'dashboard', component: Dashboard },
+];
